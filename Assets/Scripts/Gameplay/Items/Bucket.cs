44d099f0416a2fbf78825192ad582c1f4b28ba92
Peteksi <matias.cederberg@hotmail.com>
2023-10-13 17:39:39 +0300

    Bounce

--- Assets/Scripts/Gameplay/Items/Bucket.cs
+++ Assets/Scripts/Gameplay/Items/Bucket.cs
@@ -19,5 +19,20 @@ public class Bucket : ItemBase
     public override void FixedUpdateNetwork()
     {
         transform.position += Velocity * Runner.DeltaTime;
+
+        var normalizedVelocity = Velocity.normalized;
+
+        if (Runner.GetPhysicsScene().Raycast(transform.position - normalizedVelocity * .5f,
+            normalizedVelocity, out var hitInfo, 1, LayerMask.GetMask("Wall")))
+        {
+            Velocity = Vector3.Reflect(Velocity, hitInfo.normal);
+        }
     }
 }
+
+
+//var inputAuthority = Object.InputAuthority;
+//var hitboxManager = Runner.LagCompensation;
+
+//if (hitboxManager.Raycast(transform.position - normalizedVelocity * .5f, normalizedVelocity, 1,
+//inputAuthority, out var hitInfo, LayerMask.GetMask("Wall"), HitOptions.IncludePhysX))
